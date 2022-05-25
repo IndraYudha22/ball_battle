02b630126bbbbc11da5aabe6109b1d01ca0353dd
Indra  Yudha <indrayudha.ind@gmail.com>
2022-05-26 03:03:54 +0700

    add toggle AR and Visual

--- Assets/_Scripts/Managers/ARManager/ARManager.cs
+++ Assets/_Scripts/Managers/ARManager/ARManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,23 +11,42 @@ public partial class ARManager : MonoBehaviour
     [SerializeField] private GameObject cursorObject;
     [SerializeField] private GameObject objectToPlace;
     [SerializeField] private ARRaycastManager raycastManager;
+    [SerializeField] private ARPlaneManager arPlaneManager;
     [SerializeField] private GameObject previewObectToPlace;
     [SerializeField] private Button btnGenerate;
+
+    [SerializeField] private Button btnToggleAR;
+    [SerializeField] private Button btnToggleVisualizer;
+
+    private delegate void DelegateToggleAR();
+    private DelegateToggleAR delegateToggleAR;
+
+    private delegate void DelegateToggleVisual();
+    private DelegateToggleAR delegateToggleVisual;
 
     private bool useCursor = false;
     private bool placeObject = false;
     private Pose placementPose;
+
+    private Color32 colorEnable = new Color32(147, 61, 255, 255); // purple
+    private Color32 colorDisable = new Color32(73, 73, 73, 255); // grey
 
     GameObject spawnObject;
 
     void Start()
     {
         rotateObject = previewObectToPlace; // set default object for rotateObject
-        // cursorChildObject.SetActive(useCursor);
+
+        delegateToggleVisual = DisableVisual;
+        delegateToggleAR = DisableAR;
 
         btnGenerate.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "GENERATE";
-        
+
         btnGenerate.onClick.AddListener(ConditionPlayAR);
+
+        btnToggleAR.onClick.AddListener(SetToggleAR);
+        btnToggleVisualizer.onClick.AddListener(SetToggleVisual);
+        // btnToggle.onClick.AddListener();
     }
 
 
@@ -38,6 +56,70 @@ public partial class ARManager : MonoBehaviour
 
         UpdateCursor();
         UpdatePlacementIndicator();
+    }
+
+    private void SetToggleAR()
+    {
+        delegateToggleAR?.Invoke();
+        if (delegateToggleAR == DisableAR)
+        {
+            delegateToggleAR = EnableAR;
+        }
+        else if (delegateToggleAR == EnableAR)
+        {
+            delegateToggleAR = DisableAR;
+        }
+    }
+
+    private void DisableAR()
+    {
+        arPlaneManager.enabled = false;
+        raycastManager.enabled = false;
+        btnToggleAR.GetComponent<Image>().color = colorDisable;
+        btnToggleAR.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "OFF";
+    }
+
+    private void EnableAR()
+    {
+        arPlaneManager.enabled = true;
+        raycastManager.enabled = true;
+        btnToggleAR.GetComponent<Image>().color = colorEnable;
+        btnToggleAR.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "ON";
+    }
+
+    private void SetToggleVisual()
+    {
+        delegateToggleVisual?.Invoke();
+        if (delegateToggleVisual == DisableVisual)
+        {
+            delegateToggleVisual = EnableVisual;
+        }
+        else if (delegateToggleVisual == EnableVisual)
+        {
+            delegateToggleVisual = DisableVisual;
+        }
+    }
+
+    private void DisableVisual()
+    {
+        SetAllPlaneActive(false);
+        btnToggleVisualizer.GetComponent<Image>().color = colorDisable;
+        btnToggleVisualizer.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "OFF";
+    }
+
+    private void EnableVisual()
+    {
+        SetAllPlaneActive(true);
+        btnToggleVisualizer.GetComponent<Image>().color = colorEnable;
+        btnToggleVisualizer.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "ON";
+    }
+
+    private void SetAllPlaneActive(bool value)
+    {
+        foreach (var plane in arPlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(value);
+        }
     }
 
     private void ConditionPlayAR()
@@ -89,23 +171,31 @@ public partial class ARManager : MonoBehaviour
     {
         if (placeObject) return;
         Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        raycastManager.Raycast(screenPosition, hits, TrackableType.Planes);
 
-        useCursor = hits.Count > 0;
-        if (useCursor)
+        if (arPlaneManager.enabled)
         {
-            placementPose = hits[0].pose;
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            raycastManager.Raycast(screenPosition, hits, TrackableType.Planes);
 
-            cursorObject.transform.rotation = hits[0].pose.rotation;
-            cursorObject.transform.position = hits[0].pose.position;
+            useCursor = hits.Count > 0;
+            if (useCursor)
+            {
+                placementPose = hits[0].pose;
 
-            // transform.rotation = hits[0].pose.rotation;
+                cursorObject.transform.rotation = hits[0].pose.rotation;
+                cursorObject.transform.position = hits[0].pose.position;
 
-            // var cameraForward = Camera.main.transform.forward;
-            // var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+                // transform.rotation = hits[0].pose.rotation;
 
-            // placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+                // var cameraForward = Camera.main.transform.forward;
+                // var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+
+                // placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            }
+        }
+        else
+        {
+            useCursor = false;
         }
     }
 
